a04900857128bd82f05d8798cbf4827ec0424950
Alcides Fonseca <me@alcidesfonseca.com>
2020-05-21 14:34:34 +0100

    regen -> regen-api

--- regen/regen-example/src/main/java/regen/test/project/SimpleTest.java
+++ regen/regen-example/src/main/java/regen/test/project/SimpleTest.java
@@ -10,7 +10,20 @@ public class SimpleTest {
 		@Refinement("b > 3")
     	int b = 2; // should emit error
 		
-		@Refinement("c > 1")
+		@Refinement("c == 2 || c == 3 || c == 4")
     	int c = 2; // should be OK
+		
+		{
+			int d = 2;
+		}
+		
+		if ( a == 1) {
+			c = 3;
+		} else {
+			c = 4;
+		}
+		
+		@Refinement("d > 2")
+		int d = c;
     }
 }
