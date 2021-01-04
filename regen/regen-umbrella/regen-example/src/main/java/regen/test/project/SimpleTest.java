7810e93b157015403ee730a8848b599c164489a0
CatarinaGamboa <catarina.vg@hotmail.com>
2021-01-04 19:17:31 +0000

    Passing all tests

--- regen/regen-umbrella/regen-example/src/main/java/regen/test/project/SimpleTest.java
+++ regen/regen-umbrella/regen-example/src/main/java/regen/test/project/SimpleTest.java
@@ -4,29 +4,20 @@ import repair.regen.specification.Refinement;
 
 public class SimpleTest {
 	
-	public static void main(String[] args) {	
-		@Refinement("\\v > 10")
-		int a = 15;
-		if(a > 14) {
-			a = 12;
-			@Refinement("\\v < 11")
-			int c = a;
-		}
-
-		
-//		@Refinement("(z > 0) && (z < 50)")
-//    	int z = 1;
-//    	@Refinement("u < 100")
-//    	int u = 10;
-//    	u = 11 + z;
-//    	u = z*2;
-//    	u = 30 + z;
-//    	@Refinement("\\v > 0")
-//    	int n = 1;
-//    	n = z + n + 1 * n;
-//    	@Refinement("y > 0")
-//    	int y = 15;
-//    	y = y*y;
+	public static void main(String[] args) {		
+		@Refinement("(z > 0) && (z < 50)")
+    	int z = 1;
+    	@Refinement("u < 100")
+    	int u = 10;
+    	u = 11 + z;
+    	u = z*2;
+    	u = 30 + z;
+    	@Refinement("\\v > 0")
+    	int n = 1;
+    	n = z + n + 1 * n;
+    	@Refinement("y > 0")
+    	int y = 15;
+    	y = y*y;
 	}
 
 }
--- regen/regen-umbrella/regen-verifier/spooned/regen/test/project/SimpleTest.java
+++ regen/regen-umbrella/regen-verifier/spooned/regen/test/project/SimpleTest.java
@@ -3,26 +3,19 @@ package regen.test.project;
 
 public class SimpleTest {
     public static void main(java.lang.String[] args) {
-        @repair.regen.specification.Refinement("\\v > 10")
-        int a = 15;
-        if (a > 14) {
-            a = 12;
-            @repair.regen.specification.Refinement("\\v < 11")
-            int c = a;
-        }
-        // @Refinement("(z > 0) && (z < 50)")
-        // int z = 1;
-        // @Refinement("u < 100")
-        // int u = 10;
-        // u = 11 + z;
-        // u = z*2;
-        // u = 30 + z;
-        // @Refinement("\\v > 0")
-        // int n = 1;
-        // n = z + n + 1 * n;
-        // @Refinement("y > 0")
-        // int y = 15;
-        // y = y*y;
+        @repair.regen.specification.Refinement("(z > 0) && (z < 50)")
+        int z = 1;
+        @repair.regen.specification.Refinement("u < 100")
+        int u = 10;
+        u = 11 + z;
+        u = z * 2;
+        u = 30 + z;
+        @repair.regen.specification.Refinement("\\v > 0")
+        int n = 1;
+        n = (z + n) + (1 * n);
+        @repair.regen.specification.Refinement("y > 0")
+        int y = 15;
+        y = y * y;
     }
 }
 
