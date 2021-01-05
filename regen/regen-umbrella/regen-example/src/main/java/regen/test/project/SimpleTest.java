428e70d509ea1eadeec158176cbbdf04b44554de
CatarinaGamboa <catarina.vg@hotmail.com>
2021-01-05 14:09:35 +0000

    try examples

--- regen/regen-umbrella/regen-example/src/main/java/regen/test/project/SimpleTest.java
+++ regen/regen-umbrella/regen-example/src/main/java/regen/test/project/SimpleTest.java
@@ -3,15 +3,21 @@ package regen.test.project;
 import repair.regen.specification.Refinement;
 
 public class SimpleTest {
+	
+	@Refinement("{n >= 0} -> {\\v >= 0}")
+	public int fibonnaci(int n) {
+		if(n <= 2)
+			return n;
+		else
+			return fibonnaci(n-1) + fibonnaci(n-2);
+	}
 
 	public static void main(String[] args) {		
-		@Refinement("\\v > 5")
-		int a = 10;
-		@Refinement("\\v > 10")
-		int b = a+1;
-		if(b < a) {
-			a = 3;
-		}
+//		@Refinement("\\v > 5")
+//		int a = 10;
+//		@Refinement("\\v > a")
+//		int b = 11;
+		
 		
 	}
 
--- regen/regen-umbrella/regen-verifier/spooned/regen/test/project/SimpleTest.java
+++ regen/regen-umbrella/regen-verifier/spooned/regen/test/project/SimpleTest.java
@@ -2,14 +2,20 @@ package regen.test.project;
 
 
 public class SimpleTest {
+    @repair.regen.specification.Refinement("{n >= 0} -> {\\v >= 0}")
+    public int fibonnaci(int n) {
+        if (n <= 2)
+            return n;
+        else
+            return (fibonnaci((n - 1))) + (fibonnaci((n - 2)));
+
+    }
+
     public static void main(java.lang.String[] args) {
-        @repair.regen.specification.Refinement("\\v > 5")
-        int a = 10;
-        @repair.regen.specification.Refinement("\\v > 10")
-        int b = a + 1;
-        if (b < a) {
-            a = 3;
-        }
+        // @Refinement("\\v > 5")
+        // int a = 10;
+        // @Refinement("\\v > a")
+        // int b = 11;
     }
 }
 
