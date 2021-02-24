9e4edda2171d4fd344600d01cc6ccdc7a3b62340
CatarinaGamboa <catarina.vg@hotmail.com>
2021-02-24 23:18:24 +0000

    Remove unused language constructs. Alias not working with other expressions that are not variables-cannot use substituteName in this case, needs to substitute the all expression. Ex: Greater(a, 8+1) - the second variable has to be substituted by the new expression

--- regen/regen-umbrella/regen-example/src/main/java/regen/test/project/Car.java
+++ regen/regen-umbrella/regen-example/src/main/java/regen/test/project/Car.java
@@ -3,37 +3,37 @@ package regen.test.project;
 import repair.regen.specification.Refinement;
 import repair.regen.specification.RefinementAlias;
 
-//@RefinementAlias("Positive(int x) { x > 0}")
-//@RefinementAlias("type CarAcceptableYears(int x) { x > 1800 && x < 2050}")
-//public class Car {
-//	
-//	@Refinement("CarAcceptableYears(year)")
-//	private int year;
-//	
-//	@Refinement("Positive(_)")
-//	private int seats;
-//	
-//	
-//	public void setYear(@Refinement(" _ > 1900 && _ < 2000") int year) {
-//		this.year = year;
+@RefinementAlias("Positive(int x) { x > 0}")
+@RefinementAlias("type CarAcceptableYears(int x) { x > 1800 && x < 2050}")
+public class Car {
+	
+	@Refinement("CarAcceptableYears(year)")
+	private int year;
+	
+	@Refinement("Positive(_)")
+	private int seats;
+	
+	
+	public void setYear(@Refinement(" _ > 1900 && _ < 2000") int year) {
+		this.year = year;
+	}
+	
+	@Refinement("CarAcceptableYears(_)")
+	public int getYear() {
+		return year;
+	}
+	
+	
+//	@Refinement("_ == old(year) + i")
+//	public int addYear(int i) {
+//		return year + i;
 //	}
-//	
-//	@Refinement("CarAcceptableYears(_)")
-//	public int getYear() {
-//		return year;
+	
+	
+//	@Refinement("_ == year")
+//	public int getAge(int yearNow) {
+//		return (yearNow+1) - year;
 //	}
-//	
-//	
-////	@Refinement("_ == old(year) + i")
-////	public int addYear(int i) {
-////		return year + i;
-////	}
-//	
-//	
-////	@Refinement("_ == year")
-////	public int getAge(int yearNow) {
-////		return (yearNow+1) - year;
-////	}
-//	
-//
-//}
+	
+
+}
