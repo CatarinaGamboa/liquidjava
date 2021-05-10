515cdd98082f4a4f3609e9fa07a336318745702c
CatarinaGamboa <catarina.vg@hotmail.com>
2021-05-10 19:05:23 +0100

    Join error messages in stringbuilder before printing

--- regen/regen-umbrella/regen-verifier/src/main/java/repair/regen/utils/ErrorHandler.java
+++ regen/regen-umbrella/regen-verifier/src/main/java/repair/regen/utils/ErrorHandler.java
@@ -1,7 +1,9 @@
 package repair.regen.utils;
 
+import java.util.Formatter;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 import repair.regen.processor.constraints.Constraint;
 import repair.regen.processor.constraints.Predicate;
@@ -30,178 +32,187 @@ public class ErrorHandler {
 	}
 	public static <T> void printError(CtElement var, String moreInfo, Constraint expectedType, 
 			Constraint cSMT, HashMap<String, PlacementInCode> map) {		
+		StringBuilder sb = new StringBuilder();
 		
-		System.out.println("______________________________________________________");
-		System.err.println("Failed to check refinement at: ");
-		System.out.println();
+		sb.append("______________________________________________________\n");
+		sb.append("Failed to check refinement at: \n\n");
+
 		if(moreInfo!=null)
-			System.out.println(moreInfo);
-		System.out.println(var);
-		System.out.println();
-		System.out.println("Type expected:" + expectedType.toString());
-		System.out.println("Refinement found:" + cSMT.toString());
-		printMap(map);
-		System.out.println("Location: " + var.getPosition());
-		System.out.println("______________________________________________________");
+			sb.append(moreInfo+"\n");
+		sb.append(var + "\n\n");
+		sb.append("Type expected:" + expectedType.toString()+"\n");
+		sb.append("Refinement found:" + cSMT.toString()+"\n");
+		sb.append(printMap(map));
+		sb.append("Location: " + var.getPosition()+ "\n");
+		sb.append("______________________________________________________\n");
+		System.out.println(sb.toString());
 		System.exit(1);
 	}
 	
 	public static void printStateMismatch(CtElement element, String method, VCImplication constraintForErrorMsg, 
 			String states, HashMap<String, PlacementInCode> map) {
-		System.out.println("______________________________________________________");
-		System.err.println(" Failed to check state transitions when calling "+ method+" in:");
-		System.out.println();
-		System.out.println(element);
-		System.out.println();
-		System.out.println("Expected possible states:" + states);
-		System.out.println("\nState found:");
+		StringBuilder sb = new StringBuilder();
+		sb.append("______________________________________________________\n");
+		sb.append(" Failed to check state transitions when calling "+ method+" in:\n\n");
+		sb.append(element+"\n\n");
+		sb.append("Expected possible states:" + states + "\n");
+		sb.append("\nState found:\n");
 		//TODO SHOW
-		printLine();
-		System.out.println("\n"+constraintForErrorMsg/*.toConjunctions()*/.toString());
-		printLine();
-		System.out.println();
-		printMap(map);
-		System.out.println("Location: " + element.getPosition());
-		System.out.println("______________________________________________________");
+		sb.append(printLine());
+		sb.append("\n"+constraintForErrorMsg/*.toConjunctions()*/.toString()+"\n");
+		sb.append(printLine());
+		sb.append("\n");
+		sb.append(printMap(map));
+		sb.append("Location: " + element.getPosition()+"\n");
+		sb.append("______________________________________________________\n");
+		System.out.println(sb.toString());
 		System.exit(1);
 
 	}
 	
 	
 	public static <T> void printErrorUnknownVariable(CtElement var, String et, String correctRefinement, HashMap<String, PlacementInCode> map) {
-		System.out.println("______________________________________________________");
-		System.err.println("Encountered unknown variable");
-		System.out.println();
-		System.out.println(var);
-		System.out.println();
-		printMap(map);
-		System.out.println("Location: " + var.getPosition());
-		System.out.println("______________________________________________________");
+		StringBuilder sb = new StringBuilder();
+		sb.append("______________________________________________________\n");
+		sb.append("Encountered unknown variable\n\n");
+		sb.append(var+"\n\n");
+		sb.append(printMap(map));
+		sb.append("Location: " + var.getPosition()+"\n");
+		sb.append("______________________________________________________\n");
+		System.out.println(sb.toString());
 		System.exit(2);
 	}
 	
 	public static <T> void printNotFound(CtElement var, Constraint constraint, Constraint constraint2, String msg, HashMap<String, PlacementInCode> map) {
-		System.out.println("______________________________________________________");
-		System.err.println(msg);
-		System.out.println(constraint);
-		System.out.println(constraint2);
-		System.out.println();
-		System.out.println("Error found while checking conditions in:");
-		System.out.println(var);
-		System.out.println();
-		printMap(map);
-		System.out.println("Location: " + var.getPosition());
-		System.out.println("______________________________________________________");
+		StringBuilder sb = new StringBuilder();
+		sb.append("______________________________________________________\n");
+		sb.append(msg);
+		sb.append(constraint+"\n");
+		sb.append(constraint2+"\n\n");
+		sb.append("Error found while checking conditions in:\n");
+		sb.append(var+"\n\n");
+		sb.append(printMap(map));
+		sb.append("Location: " + var.getPosition()+"\n");
+		sb.append("______________________________________________________\n");
+		System.out.println(sb.toString());
 		System.exit(2);
 	}
 	
 	
 	public static <T> void printErrorArgs(CtElement var, Constraint expectedType, String msg, HashMap<String, PlacementInCode> map) {
-		System.out.println("______________________________________________________");
-		System.err.println("Error in ghost invocation: "+ msg);
-		System.out.println(var+"\nError in refinement:" + expectedType.toString());
-		printMap(map);
-		System.out.println("Location: " + var.getPosition());
-		System.out.println("______________________________________________________");
+		StringBuilder sb = new StringBuilder();
+		sb.append("______________________________________________________\n");
+		sb.append("Error in ghost invocation: "+ msg+"\n");
+		sb.append(var+"\nError in refinement:" + expectedType.toString()+"\n");
+		sb.append(printMap(map));
+		sb.append("Location: " + var.getPosition()+"\n");
+		sb.append("______________________________________________________\n");
+		System.out.println(sb.toString());
 		System.exit(2);
 	}
 
 	public static void printErrorTypeMismatch(CtElement element, Constraint expectedType, String message, HashMap<String, PlacementInCode> map) {
-		System.out.println("______________________________________________________");
-		System.err.println(message);
-		System.out.println();
-		System.out.println(element);
-		printMap(map);
-		System.out.println("Location: " + element.getPosition());
-		System.out.println("______________________________________________________");
+		StringBuilder sb = new StringBuilder();
+		sb.append("______________________________________________________\n");
+		sb.append(message+"\n\n");
+		sb.append(element+"\n");
+		sb.append(printMap(map));
+		sb.append("Location: " + element.getPosition() + "\n");
+		sb.append("______________________________________________________\n");
+		System.out.println(sb.toString());
 		System.exit(2);
 
 	}
 
 	public static void printSameStateSetError(CtElement element, Constraint p,String name, HashMap<String, PlacementInCode> map) {
-		System.out.println("______________________________________________________");
-		System.err.println(" Error found multiple disjoint states from a State Set in a refinement");
-		System.out.println();
-		System.out.println(element);
-		System.out.println();
-		System.out.println("In predicate:" + p.toString());
-		System.out.println("In class:" + name);
-		printMap(map);
-		System.out.println("Location: " + element.getPosition());
-		System.out.println("______________________________________________________");
+		StringBuilder sb = new StringBuilder();
+		sb.append("______________________________________________________\n");
+		sb.append("Error found multiple disjoint states from a State Set in a refinement\n\n");
+		sb.append(element+"\n\n");
+		sb.append("In predicate:" + p.toString()+"\n");
+		sb.append("In class:" + name+"\n");
+		sb.append(printMap(map));
+		sb.append("Location: " + element.getPosition()+"\n");
+		sb.append("______________________________________________________\n");
+		System.out.println(sb.toString());
 		System.exit(1);
-		
 	}
 
 	public static void printErrorConstructorFromState(CtElement element, CtLiteral<String> from) {
-		System.out.println("______________________________________________________");
-		System.err.println(" Error found constructor with FROM state (Constructor's should only have a TO state)");
-		System.out.println();
-		System.out.println(element);
-		System.out.println();
-		System.out.println("State found:" + from);
-		System.out.println("Location: " + element.getPosition());
-		System.out.println("______________________________________________________");
+		StringBuilder sb = new StringBuilder();
+		sb.append("______________________________________________________\n");
+		sb.append(" Error found constructor with FROM state (Constructor's should only have a TO state)\n\n");
+		sb.append(element+"\n\n");
+		sb.append("State found:" + from + "\n");
+		sb.append("Location: " + element.getPosition() + "\n");
+		sb.append("______________________________________________________\n");
+		System.out.println(sb.toString());
 		System.exit(1);
 		
 	}
 	
 	public static void printCostumeError(CtElement element, String msg) {
-		System.out.println("______________________________________________________");
-		System.err.println(" Found Error: "+msg);
-		System.out.println();
-		System.out.println(element);
-		System.out.println();
-		System.out.println("Location: " + element.getPosition());
-		System.out.println("______________________________________________________");
-		System.exit(1);
-		
+		StringBuilder sb = new StringBuilder();
+		sb.append("______________________________________________________\n");
+		sb.append(" Found Error: "+msg+"\n\n");
+		sb.append(element+"\n\n");
+		sb.append("Location: " + element.getPosition()+"\n");
+		sb.append("______________________________________________________\n");
+		sb.append(sb.toString());
+		System.out.println(sb.toString());
+		System.exit(1);	
 	}
 
 	
 	public static void printSyntaxError(String msg, String ref, CtElement element) {
-		System.out.println("______________________________________________________");
-		System.err.println("Syntax error with message");
-		System.out.println(msg);
-		System.out.println("Found in refinement:");
-		System.out.println(ref);
-		System.out.println("In:");
-		System.out.println(element);
-		System.out.println("Location: " + element.getPosition());
-		System.out.println("______________________________________________________");
+		StringBuilder sb = new StringBuilder();
+		sb.append("______________________________________________________\n");
+		sb.append("Syntax error with message\n");
+		sb.append(msg+"\n");
+		sb.append("Found in refinement:\n");
+		sb.append(ref+"\n");
+		sb.append("In:\n");
+		sb.append(element+"\n");
+		sb.append("Location: " + element.getPosition()+"\n");
+		sb.append("______________________________________________________\n");
+		System.out.println(sb.toString());
 		System.exit(2);
 		
 	}
 	
 	public static void printSyntaxError(String msg, String ref) {
-		System.out.println("______________________________________________________");
-		System.err.println("Syntax error with message");
-		System.out.println(msg);
-		System.out.println("Found in refinement:");
-		System.out.println(ref);
-		System.out.println("______________________________________________________");
+		StringBuilder sb = new StringBuilder();
+		sb.append("______________________________________________________\n");
+		sb.append("Syntax error with message\n");
+		sb.append(msg+"\n");
+		sb.append("Found in refinement:\n");
+		sb.append(ref+"\n");
+		sb.append("______________________________________________________\n");
+		System.out.println(sb.toString());
 		System.exit(2);
 		
 	}
 	
-	private static void printLine() {
-		for (int i = 0; i < 130; i++) System.out.print("-");//-----------
+	private static String printLine() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 130; i++) sb.append("-");//-----------
+		return sb.toString();
 	}
 	
-	private static void printMap(HashMap<String, PlacementInCode> map) {
-		if(map.isEmpty()) return;
-		System.out.println("\nInstance translation table:");
-		printLine();
+	private static String printMap(HashMap<String, PlacementInCode> map) {
+		StringBuilder sb = new StringBuilder();
+		Formatter formatter = new Formatter(sb, Locale.US);
+		if(map.isEmpty()) return "";
+		formatter.format("\nInstance translation table:\n");
+		formatter.format(printLine());
 		//title
-		System.out.format("\n| %-32s | %-60s | %-1s \n", "Variable Name", "Created in", "File");
-		printLine();
+		formatter.format("\n| %-32s | %-60s | %-1s \n", "Variable Name", "Created in", "File");
+		formatter.format(printLine()+"\n");
 		//data
-		System.out.println();
 		for(String s : map.keySet())
-			System.out.format("| %-32s | %-60s | %-1s \n", s, map.get(s).getText(), map.get(s).getSimplePosition());
+			formatter.format("| %-32s | %-60s | %-1s \n", s, map.get(s).getText(), map.get(s).getSimplePosition());
 		//end
-		printLine();
-		System.out.println();
-		
+		formatter.format(printLine()+"\n\n");
+		return formatter.toString();
 	}
 }
