578a1e708793581279b74341da0acc66fc71126a
CatarinaGamboa <catarina.vg@hotmail.com>
2021-05-31 18:24:34 +0100

    Remove null initialization

--- evaluation_survey/test-projects/part3-liquidJava/sum/src/sum/Test.java
+++ evaluation_survey/test-projects/part3-liquidJava/sum/src/sum/Test.java
@@ -12,12 +12,17 @@ public class Test {
     */
     @Refinement("Nat(_) && _ >= n")
 	  public static int sum(int n) {
-		  if(n <= 1)//correct: 0
+		  if(n <= 0)//correct: 0
 		  	return 0;
 		  else {
 			  int t1 = sum(n-1);
 			  return n + t1;
 		  }
   	}
+
+	  public static void main(String[] args) {
+		@Refinement("a > 10")
+		int a = 10;	  
+	  }
     
 }
