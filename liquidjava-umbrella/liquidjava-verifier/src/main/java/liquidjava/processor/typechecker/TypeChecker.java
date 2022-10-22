8a68ccd967ba0a3297e30adfd624849a4245e81b
Kirill Golubev <frogofjuly@Kirills-MacBook-Air.local>
2022-10-23 00:31:13 +0300

    idea

---
+++ liquidjava-umbrella/liquidjava-verifier/src/main/java/liquidjava/processor/typechecker/TypeChecker.java
@@ -0,0 +1,14 @@
+package liquidjava.processor.typechecker;
+
+public class TypeChecker {
+    /*
+    * My idea is to separate typing rules to external class.
+    * This will unbind typing from traversing java syntax tree.
+    * Also, it will make adding new typing rules easier.
+    *
+    * There is already a TypeChecker class, but I want to make it closer to
+    * formalization of Liquid Java.
+    *
+    * I do not know if it is possible or useful, so I am creating this pull request to discuss.
+    * */
+}
