e62a0e6fa69044d4fb7354e30bbdefa22590c15c
Kirill Golubev <frogofjuly@Kirills-MacBook-Air.local>
2023-04-06 00:23:09 +0300

    disabling tests

--- liquidjava-umbrella/liquidjava-verifier/src/main/java/liquidjava/api/tests/TestSeparationLogic.java
+++ liquidjava-umbrella/liquidjava-verifier/src/main/java/liquidjava/api/tests/TestSeparationLogic.java
@@ -3,8 +3,8 @@ package liquidjava.api.tests;
 import org.junit.Test;
 
 public class TestSeparationLogic extends TestAbstract {
-    @Test
-    public void dummyTest() {
-        testCorrect(testPath + "separation_logic/Dummy.java");
-    }
+    // @Test
+    // public void dummyTest() {
+    // testCorrect(testPath + "separation_logic/Dummy.java");
+    // }
 }
