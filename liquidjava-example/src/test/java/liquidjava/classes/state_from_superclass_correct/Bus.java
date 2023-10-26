4ffb91aa5d8cc62393eaa944ebc1851becc88e05
Alcides Fonseca <me@alcidesfonseca.com>
2023-10-26 23:58:32 +0100

    Removed TODO

--- liquidjava-example/src/test/java/liquidjava/classes/state_from_superclass_correct/Bus.java
+++ liquidjava-example/src/test/java/liquidjava/classes/state_from_superclass_correct/Bus.java
@@ -5,17 +5,14 @@ import liquidjava.specification.StateRefinement;
 public class Bus extends Car {
 
   @StateRefinement(to = "close(this)")
-  public Bus() {}
+  public Bus() {
+  }
 
   @Override
   public void open() {
-    // TODO Auto-generated method stub
-
   }
 
   @Override
   public void close() {
-    // TODO Auto-generated method stub
-
   }
 }
