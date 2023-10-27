423dbd7efb0b715408cb444d97b36a7f450f1b0e
Catarina Gamboa <catarina.vg@hotmail.com>
2023-10-27 04:38:58 -0400

    change test verification by using error emitter

--- liquidjava-verifier/src/main/java/liquidjava/api/tests/TestAbstract.java
+++ liquidjava-verifier/src/main/java/liquidjava/api/tests/TestAbstract.java
@@ -4,25 +4,21 @@ import static org.junit.Assert.fail;
 
 import java.security.Permission;
 import liquidjava.api.CommandLineLauncher;
+import liquidjava.errors.ErrorEmitter;
 
 public abstract class TestAbstract {
     String testPath = "../liquidjava-example/src/test/java/liquidjava/";
 
     protected void testCorrect(String filename) {
-        try {
-            CommandLineLauncher.launchTest(filename);
-        } catch (SecurityException e) {
-            fail();
-        }
+    	ErrorEmitter errorEmitter = CommandLineLauncher.launchTest(filename);
+        if(errorEmitter.foundError())
+        	fail();
     }
 
     protected void testWrong(String filename) {
-        try {
-            CommandLineLauncher.launchTest(filename);
-        } catch (SecurityException e) {
-            return;
-        }
-        fail();
+        ErrorEmitter errorEmitter = CommandLineLauncher.launchTest(filename);
+        if(!errorEmitter.foundError())
+        	fail();
     }
 
     class MySecurityManager extends SecurityManager {
