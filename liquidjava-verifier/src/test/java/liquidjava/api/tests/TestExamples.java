352f10e6f4864c8f6dfe8db4691c6ccf364fce45
Alcides Fonseca <me@alcidesfonseca.com>
2023-10-28 15:14:52 +0100

    Reorganized files

---
+++ liquidjava-verifier/src/test/java/liquidjava/api/tests/TestExamples.java
@@ -0,0 +1,29 @@
+package liquidjava.api.tests;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import liquidjava.api.CommandLineLauncher;
+import liquidjava.errors.ErrorEmitter;
+
+public class TestExamples {
+    public void testFile() {
+        try {
+            Files.find(Paths.get("../liquidjava-example/src/main/java/"), Integer.MAX_VALUE,
+                    (filePath, fileAttr) -> fileAttr.isRegularFile()).forEach(f -> {
+                        ErrorEmitter errorEmitter = CommandLineLauncher.launchTest(f.toAbsolutePath().toString());
+                        if (f.startsWith("Correct") && errorEmitter.foundError()) {
+                            fail();
+                        }
+                        if (f.startsWith("Error") && !errorEmitter.foundError()) {
+                            fail();
+                        }
+                    });
+        } catch (IOException e) {
+            assert false;
+        }
+    }
+}
