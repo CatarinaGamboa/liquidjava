9ef9595b6e72442673ecab4a4464c501425e6099
Ricardo Costa <rcosta.ms358@gmail.com>
2025-10-02 20:33:18 +0100

    Improve Switch Cases

--- liquidjava-verifier/src/main/java/liquidjava/rj_language/opt/ConstantFolding.java
+++ liquidjava-verifier/src/main/java/liquidjava/rj_language/opt/ConstantFolding.java
@@ -38,120 +38,74 @@ public class ConstantFolding {
             int l = ((LiteralInt) left).getValue();
             int r = ((LiteralInt) right).getValue();
 
-            switch (op) {
-                case "+":
-                    return new LiteralInt(l + r);
-                case "-":
-                    return new LiteralInt(l - r);
-                case "*":
-                    return new LiteralInt(l * r);
-                case "/":
-                    if (r != 0)
-                        return new LiteralInt(l / r);
-                    break;
-                case "%":
-                    if (r != 0)
-                        return new LiteralInt(l % r);
-                    break;
-                case "<":
-                    return new LiteralBoolean(l < r);
-                case "<=":
-                    return new LiteralBoolean(l <= r);
-                case ">":
-                    return new LiteralBoolean(l > r);
-                case ">=":
-                    return new LiteralBoolean(l >= r);
-                case "==":
-                    return new LiteralBoolean(l == r);
-                case "!=":
-                    return new LiteralBoolean(l != r);
-            }
+            return switch (op) {
+                case "+" -> new LiteralInt(l + r);
+                case "-" -> new LiteralInt(l - r);
+                case "*" -> new LiteralInt(l * r);
+                case "/" -> r != 0 ? new LiteralInt(l / r) : binExp;
+                case "%" -> r != 0 ? new LiteralInt(l % r) : binExp;
+                case "<" -> new LiteralBoolean(l < r);
+                case "<=" -> new LiteralBoolean(l <= r);
+                case ">" -> new LiteralBoolean(l > r);
+                case ">=" -> new LiteralBoolean(l >= r);
+                case "==" -> new LiteralBoolean(l == r);
+                case "!=" -> new LiteralBoolean(l != r);
+                default -> binExp;
+            };
         }
 
         // arithmetic operations with real literals
         else if (left instanceof LiteralReal && right instanceof LiteralReal) {
             double l = ((LiteralReal) left).getValue();
             double r = ((LiteralReal) right).getValue();
-            switch (op) {
-                case "+":
-                    return new LiteralReal(l + r);
-                case "-":
-                    return new LiteralReal(l - r);
-                case "*":
-                    return new LiteralReal(l * r);
-                case "/":
-                    if (r != 0.0)
-                        return new LiteralReal(l / r);
-                    break;
-                case "%":
-                    if (r != 0.0)
-                        return new LiteralReal(l % r);
-                    break;
-                case "<":
-                    return new LiteralBoolean(l < r);
-                case "<=":
-                    return new LiteralBoolean(l <= r);
-                case ">":
-                    return new LiteralBoolean(l > r);
-                case ">=":
-                    return new LiteralBoolean(l >= r);
-                case "==":
-                    return new LiteralBoolean(l == r);
-                case "!=":
-                    return new LiteralBoolean(l != r);
-            }
+            return switch (op) {
+                case "+" -> new LiteralReal(l + r);
+                case "-" -> new LiteralReal(l - r);
+                case "*" -> new LiteralReal(l * r);
+                case "/" -> r != 0.0 ? new LiteralReal(l / r) : binExp;
+                case "%" -> r != 0.0 ? new LiteralReal(l % r) : binExp;
+                case "<" -> new LiteralBoolean(l < r);
+                case "<=" -> new LiteralBoolean(l <= r);
+                case ">" -> new LiteralBoolean(l > r);
+                case ">=" -> new LiteralBoolean(l >= r);
+                case "==" -> new LiteralBoolean(l == r);
+                case "!=" -> new LiteralBoolean(l != r);
+                default -> binExp;
+            };
         }
 
         // mixed integer and real operations
         else if ((left instanceof LiteralInt && right instanceof LiteralReal) || (left instanceof LiteralReal && right instanceof LiteralInt)) {
             double l = left instanceof LiteralInt ? ((LiteralInt) left).getValue() : ((LiteralReal) left).getValue();
             double r = right instanceof LiteralInt ? ((LiteralInt) right).getValue() : ((LiteralReal) right).getValue();
-            switch (op) {
-                case "+":
-                    return new LiteralReal(l + r);
-                case "-":
-                    return new LiteralReal(l - r);
-                case "*":
-                    return new LiteralReal(l * r);
-                case "/":
-                    if (r != 0.0)
-                        return new LiteralReal(l / r);
-                    break;
-                case "%":
-                    if (r != 0.0)
-                        return new LiteralReal(l % r);
-                    break;
-                case "<":
-                    return new LiteralBoolean(l < r);
-                case "<=":
-                    return new LiteralBoolean(l <= r);
-                case ">":
-                    return new LiteralBoolean(l > r);
-                case ">=":
-                    return new LiteralBoolean(l >= r);
-                case "==":
-                    return new LiteralBoolean(l == r);
-                case "!=":
-                    return new LiteralBoolean(l != r);
-            }
+            return switch (op) {
+                case "+" -> new LiteralReal(l + r);
+                case "-" -> new LiteralReal(l - r);
+                case "*" -> new LiteralReal(l * r);
+                case "/" -> r != 0.0 ? new LiteralReal(l / r) : binExp;
+                case "%" -> r != 0.0 ? new LiteralReal(l % r) : binExp;
+                case "<" -> new LiteralBoolean(l < r);
+                case "<=" -> new LiteralBoolean(l <= r);
+                case ">" -> new LiteralBoolean(l > r);
+                case ">=" -> new LiteralBoolean(l >= r);
+                case "==" -> new LiteralBoolean(l == r);
+                case "!=" -> new LiteralBoolean(l != r);
+                default -> binExp;
+            };
         }
 
         // boolean operations with boolean literals
         else if (left instanceof LiteralBoolean && right instanceof LiteralBoolean) {
             boolean l = ((LiteralBoolean) left).isBooleanTrue();
             boolean r = ((LiteralBoolean) right).isBooleanTrue();
-            switch (op) {
-            case "&&":
-                return new LiteralBoolean(l && r);
-            case "||":
-                return new LiteralBoolean(l || r);
-            case "-->": // implication: !a || b
-                return new LiteralBoolean(!l || r);
-            case "==":
-                return new LiteralBoolean(l == r);
-            case "!=":
-                return new LiteralBoolean(l != r);
-            }
+            return switch (op) {
+                case "&&" -> new LiteralBoolean(l && r);
+                case "||" -> new LiteralBoolean(l || r);
+                case "-->" -> new LiteralBoolean(!l || r);
+                case "==" -> new LiteralBoolean(l == r);
+                case "!=" -> new LiteralBoolean(l != r);
+                default -> binExp;
+            };
         }
         // no folding, return original
         return binExp;
--- liquidjava-verifier/src/main/java/liquidjava/rj_language/opt/LogicSimplifier.java
+++ liquidjava-verifier/src/main/java/liquidjava/rj_language/opt/LogicSimplifier.java
@@ -41,168 +41,177 @@ public class LogicSimplifier {
         Expression right = binExp.getSecondOperand();
         String operator = binExp.getOperator();
 
-        // boolean logic simplifications
-        if (operator.equals("&&")) {
-            // true && x => x
-            if (left instanceof LiteralBoolean && ((LiteralBoolean) left).isBooleanTrue()) {
-                return right;
+        switch (operator) {
+            // logical simplifications
+            case "&&" -> {
+                // true && x => x
+                if (left instanceof LiteralBoolean && ((LiteralBoolean) left).isBooleanTrue()) {
+                    return right;
+                }
+                // x && true => x
+                if (right instanceof LiteralBoolean && ((LiteralBoolean) right).isBooleanTrue()) {
+                    return left;
+                }
+                // false && x => false
+                if (left instanceof LiteralBoolean && !((LiteralBoolean) left).isBooleanTrue()) {
+                    return new LiteralBoolean(false);
+                }
+                // x && false => false
+                if (right instanceof LiteralBoolean && !((LiteralBoolean) right).isBooleanTrue()) {
+                    return new LiteralBoolean(false);
+                }
             }
-            // x && true => x
-            if (right instanceof LiteralBoolean && ((LiteralBoolean) right).isBooleanTrue()) {
-                return left;
+            case "||" -> {
+                // true || x => true
+                if (left instanceof LiteralBoolean && ((LiteralBoolean) left).isBooleanTrue()) {
+                    return new LiteralBoolean(true);
+                }
+                // x || true => true
+                if (right instanceof LiteralBoolean && ((LiteralBoolean) right).isBooleanTrue()) {
+                    return new LiteralBoolean(true);
+                }
+                // false || x => x
+                if (left instanceof LiteralBoolean && !((LiteralBoolean) left).isBooleanTrue()) {
+                    return right;
+                }
+                // x || false => x
+                if (right instanceof LiteralBoolean && !((LiteralBoolean) right).isBooleanTrue()) {
+                    return left;
+                }
             }
-            // false && x => false
-            if (left instanceof LiteralBoolean && !((LiteralBoolean) left).isBooleanTrue()) {
-                return new LiteralBoolean(false);
+            case "-->" -> {
+                // false --> x => true
+                if (left instanceof LiteralBoolean && !((LiteralBoolean) left).isBooleanTrue()) {
+                    return new LiteralBoolean(true);
+                }
+                // x --> true => true
+                if (right instanceof LiteralBoolean && ((LiteralBoolean) right).isBooleanTrue()) {
+                    return new LiteralBoolean(true);
+                }
+                // true --> x => x
+                if (left instanceof LiteralBoolean && ((LiteralBoolean) left).isBooleanTrue()) {
+                    return right;
+                }
             }
-            // x && false => false
-            if (right instanceof LiteralBoolean && !((LiteralBoolean) right).isBooleanTrue()) {
-                return new LiteralBoolean(false);
+            // arithmetic simplifications
+            case "+" -> {
+                // 0 + x => x
+                if (left instanceof LiteralInt && ((LiteralInt) left).getValue() == 0) {
+                    return right;
+                }
+                // x + 0 => x
+                if (right instanceof LiteralInt && ((LiteralInt) right).getValue() == 0) {
+                    return left;
+                }
+                // 0.0 + x => x
+                if (left instanceof LiteralReal && ((LiteralReal) left).getValue() == 0.0) {
+                    return right;
+                }
+                // x + 0.0 => x
+                if (right instanceof LiteralReal && ((LiteralReal) right).getValue() == 0.0) {
+                    return left;
+                }
             }
-        } else if (operator.equals("||")) {
-            // true || x => true
-            if (left instanceof LiteralBoolean && ((LiteralBoolean) left).isBooleanTrue()) {
-                return new LiteralBoolean(true);
+            case "-" -> {
+                // x - 0 => x
+                if (right instanceof LiteralInt && ((LiteralInt) right).getValue() == 0) {
+                    return left;
+                }
+                // x - 0.0 => x
+                if (right instanceof LiteralReal && ((LiteralReal) right).getValue() == 0.0) {
+                    return left;
+                }
             }
-            // x || true => true
-            if (right instanceof LiteralBoolean && ((LiteralBoolean) right).isBooleanTrue()) {
-                return new LiteralBoolean(true);
+            case "*" -> {
+                // 1 * x => x
+                if (left instanceof LiteralInt && ((LiteralInt) left).getValue() == 1) {
+                    return right;
+                }
+                // x * 1 => x
+                if (right instanceof LiteralInt && ((LiteralInt) right).getValue() == 1) {
+                    return left;
+                }
+                // 0 * x => 0
+                if (left instanceof LiteralInt && ((LiteralInt) left).getValue() == 0) {
+                    return new LiteralInt(0);
+                }
+                // x * 0 => 0
+                if (right instanceof LiteralInt && ((LiteralInt) right).getValue() == 0) {
+                    return new LiteralInt(0);
+                }
+                // 1.0 * x => x
+                if (left instanceof LiteralReal && ((LiteralReal) left).getValue() == 1.0) {
+                    return right;
+                }
+                // x * 1.0 => x
+                if (right instanceof LiteralReal && ((LiteralReal) right).getValue() == 1.0) {
+                    return left;
+                }
+                // 0.0 * x => 0.0
+                if (left instanceof LiteralReal && ((LiteralReal) left).getValue() == 0.0) {
+                    return new LiteralReal(0.0);
+                }
+                // x * 0.0 => 0.0
+                if (right instanceof LiteralReal && ((LiteralReal) right).getValue() == 0.0) {
+                    return new LiteralReal(0.0);
+                }
             }
-            // false || x => x
-            if (left instanceof LiteralBoolean && !((LiteralBoolean) left).isBooleanTrue()) {
-                return right;
+            case "/" -> {
+                // x / 1 => x
+                if (right instanceof LiteralInt && ((LiteralInt) right).getValue() == 1) {
+                    return left;
+                }
+                // x / 1.0 => x
+                if (right instanceof LiteralReal && ((LiteralReal) right).getValue() == 1.0) {
+                    return left;
+                }
+                // 0 / x => 0 (assuming x != 0)
+                if (left instanceof LiteralInt && ((LiteralInt) left).getValue() == 0) {
+                    return new LiteralInt(0);
+                }
+                // 0.0 / x => 0.0 (assuming x != 0)
+                if (left instanceof LiteralReal && ((LiteralReal) left).getValue() == 0.0) {
+                    return new LiteralReal(0.0);
+                }
             }
-            // x || false => x
-            if (right instanceof LiteralBoolean && !((LiteralBoolean) right).isBooleanTrue()) {
-                return left;
+            // comparison simplifications
+            case "==" -> {
+                // x == x => true
+                if (left.equals(right)) {
+                    return new LiteralBoolean(true);
+                }
             }
-        } else if (operator.equals("-->")) {
-            // false --> x => true
-            if (left instanceof LiteralBoolean && !((LiteralBoolean) left).isBooleanTrue()) {
-                return new LiteralBoolean(true);
-            }
-            // x --> true => true
-            if (right instanceof LiteralBoolean && ((LiteralBoolean) right).isBooleanTrue()) {
-                return new LiteralBoolean(true);
-            }
-            // true --> x => x
-            if (left instanceof LiteralBoolean && ((LiteralBoolean) left).isBooleanTrue()) {
-                return right;
+            case "!=" -> {
+                // x != x => false
+                if (left.equals(right)) {
+                    return new LiteralBoolean(false);
+                }
             }
         }
-
-        // arithmetic identity simplifications
-        else if (operator.equals("+")) {
-            // 0 + x => x
-            if (left instanceof LiteralInt && ((LiteralInt) left).getValue() == 0) {
-                return right;
-            }
-            // x + 0 => x
-            if (right instanceof LiteralInt && ((LiteralInt) right).getValue() == 0) {
-                return left;
-            }
-            // 0.0 + x => x
-            if (left instanceof LiteralReal && ((LiteralReal) left).getValue() == 0.0) {
-                return right;
-            }
-            // x + 0.0 => x
-            if (right instanceof LiteralReal && ((LiteralReal) right).getValue() == 0.0) {
-                return left;
-            }
-        } else if (operator.equals("-")) {
-            // x - 0 => x
-            if (right instanceof LiteralInt && ((LiteralInt) right).getValue() == 0) {
-                return left;
-            }
-            // x - 0.0 => x
-            if (right instanceof LiteralReal && ((LiteralReal) right).getValue() == 0.0) {
-                return left;
-            }
-        } else if (operator.equals("*")) {
-            // 1 * x => x
-            if (left instanceof LiteralInt && ((LiteralInt) left).getValue() == 1) {
-                return right;
-            }
-            // x * 1 => x
-            if (right instanceof LiteralInt && ((LiteralInt) right).getValue() == 1) {
-                return left;
-            }
-            // 0 * x => 0
-            if (left instanceof LiteralInt && ((LiteralInt) left).getValue() == 0) {
-                return new LiteralInt(0);
-            }
-            // x * 0 => 0
-            if (right instanceof LiteralInt && ((LiteralInt) right).getValue() == 0) {
-                return new LiteralInt(0);
-            }
-            // 1.0 * x => x
-            if (left instanceof LiteralReal && ((LiteralReal) left).getValue() == 1.0) {
-                return right;
-            }
-            // x * 1.0 => x
-            if (right instanceof LiteralReal && ((LiteralReal) right).getValue() == 1.0) {
-                return left;
-            }
-            // 0.0 * x => 0.0
-            if (left instanceof LiteralReal && ((LiteralReal) left).getValue() == 0.0) {
-                return new LiteralReal(0.0);
-            }
-            // x * 0.0 => 0.0
-            if (right instanceof LiteralReal && ((LiteralReal) right).getValue() == 0.0) {
-                return new LiteralReal(0.0);
-            }
-        } else if (operator.equals("/")) {
-            // x / 1 => x
-            if (right instanceof LiteralInt && ((LiteralInt) right).getValue() == 1) {
-                return left;
-            }
-            // x / 1.0 => x
-            if (right instanceof LiteralReal && ((LiteralReal) right).getValue() == 1.0) {
-                return left;
-            }
-            // 0 / x => 0 (assuming x != 0)
-            if (left instanceof LiteralInt && ((LiteralInt) left).getValue() == 0) {
-                return new LiteralInt(0);
-            }
-            // 0.0 / x => 0.0 (assuming x != 0)
-            if (left instanceof LiteralReal && ((LiteralReal) left).getValue() == 0.0) {
-                return new LiteralReal(0.0);
-            }
-        }
-
-        // comparison simplifications
-        else if (operator.equals("==")) {
-            // x == x => true
-            if (left.equals(right)) {
-                return new LiteralBoolean(true);
-            }
-        } else if (operator.equals("!=")) {
-            // x != x => false
-            if (left.equals(right)) {
-                return new LiteralBoolean(false);
-            }
-        }
-        // no simplification applied
+        // no simplification
         return binExp;
     }
 
     private static Expression simplifyUnaryExpression(UnaryExpression unaryExpr) {
         Expression operand = unaryExpr.getChildren().get(0);
         String operator = unaryExpr.getOp();
-        if (operator.equals("!")) {
-            // !!x => x (double negation)
-            if (operand instanceof UnaryExpression) {
-                UnaryExpression innerUnary = (UnaryExpression) operand;
-                if (innerUnary.getOp().equals("!")) {
-                    return innerUnary.getChildren().get(0);
+        switch (operator) {
+            case "!" -> {
+                // !!x => x (double negation)
+                if (operand instanceof UnaryExpression) {
+                    UnaryExpression innerUnary = (UnaryExpression) operand;
+                    if (innerUnary.getOp().equals("!")) {
+                        return innerUnary.getChildren().get(0);
+                    }
                 }
             }
-        } else if (operator.equals("-")) {
-            // -(-x) => x (double negation)
-            if (operand instanceof UnaryExpression) {
-                UnaryExpression innerUnary = (UnaryExpression) operand;
-                if (innerUnary.getOp().equals("-")) {
-                    return innerUnary.getChildren().get(0);
+            case "-" -> {
+                // -(-x) => x (double negation)
+                if (operand instanceof UnaryExpression) {
+                    UnaryExpression innerUnary = (UnaryExpression) operand;
+                    if (innerUnary.getOp().equals("-")) {
+                        return innerUnary.getChildren().get(0);
+                    }
                 }
             }
         }
