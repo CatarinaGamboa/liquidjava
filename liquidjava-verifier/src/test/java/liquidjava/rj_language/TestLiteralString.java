b5d3912aefcf19ca6459f99d049a814f9cd24116
TORebelo <antoniorebelo1404@gmail.com>
2025-10-30 17:08:20 +0000

    test: add TestLiteralStringEquals and TestOptimizationIntegration Antonio Rebelo 58530

--- liquidjava-verifier/src/test/java/liquidjava/rj_language/TestLiteralString.java
+++ liquidjava-verifier/src/test/java/liquidjava/rj_language/TestLiteralString.java
@@ -16,38 +16,4 @@ public class TestLiteralString {
         LiteralString s2 = new LiteralString("world");
         assertNotEquals(s1.hashCode(), s2.hashCode());
     }
-
-    @Test
-    public void testEqualsSameContent() {
-        LiteralString a = new LiteralString("hello");
-        LiteralString b = new LiteralString("hello");
-        assertTrue(a.equals(b));
-        assertTrue(b.equals(a));
-    }
-
-    @Test
-    public void testEqualsDifferentContent() {
-        LiteralString a = new LiteralString("hello");
-        LiteralString c = new LiteralString("world");
-        assertFalse(a.equals(c));
-        assertFalse(c.equals(a));
-    }
-
-    @Test
-    public void testEqualsSameReference() {
-        LiteralString a = new LiteralString("hello");
-        assertTrue(a.equals(a));
-    }
-
-    @Test
-    public void testEqualsNull() {
-        LiteralString a = new LiteralString("hello");
-        assertFalse(a.equals(null));
-    }
-
-    @Test
-    public void testEqualsDifferentClass() {
-        LiteralString a = new LiteralString("hello");
-        assertFalse(a.equals(new Object()));
-    }
 }
