7c9aef7395cf814d73b0ef4538559d490047dfeb
d1niss <dinismbraposo3@gmail.com>
2025-11-06 20:47:43 +0000

    Add new unit and integration tests (fc63776)

---
+++ liquidjava-verifier/src/test/java/liquidjava/ast/opt/TestOptimizationInt.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import liquidjava.rj_language.ast.BinaryExpression;
+import liquidjava.rj_language.ast.LiteralInt;
+import liquidjava.rj_language.opt.ConstantFolding;
+import liquidjava.rj_language.opt.derivation_node.ValDerivationNode;
+
+public class TestOptimizationInt {
+    @Test
+    public void testIntegerAddFold() {
+        BinaryExpression b = new BinaryExpression(new LiteralInt(2), "+", new LiteralInt(3));
+
+        ValDerivationNode r = ConstantFolding.fold(new ValDerivationNode(b, null));
+        assertEquals(new LiteralInt(5), r.getValue());
+    }
+}
