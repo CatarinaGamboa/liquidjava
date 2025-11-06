aa2b1ceb5d18ebe97afd8093b1bee9c7aae9b155
fc63747 <fc63747@alunos.fc.ul.pt>
2025-11-06 19:54:54 +0000

    Javadocs acrescentados aos testes

--- liquidjava-verifier/src/test/java/liquidjava/rj_language/opt/TestCommandLineLauncher.java
+++ liquidjava-verifier/src/test/java/liquidjava/rj_language/opt/TestCommandLineLauncher.java
@@ -1,6 +1,9 @@
 package liquidjava.api;
 
 import org.junit.jupiter.api.Test;
+/**
+*Testa se a execução do método principal de CommandLineLauncher sem *argumentos arranca sem lançar exceções
+*/
 
 class TestCommandLineLauncher {
 
--- liquidjava-verifier/src/test/java/liquidjava/rj_language/opt/TestLiteralBoolean.java
+++ liquidjava-verifier/src/test/java/liquidjava/rj_language/opt/TestLiteralBoolean.java
@@ -3,6 +3,10 @@ package liquidjava.rj_language.ast;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+*Testa o método LiteralBoolean devolve a string "true" quando o valor booleano interno é verdadeiro
+*/
+
 class LiteralBooleanSimplestTest {
 
     @Test
