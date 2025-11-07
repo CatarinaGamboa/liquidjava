2e9f7a4ff2c49d7a3d23849634e12589525b8906
N0bree <nmnobre2005@gmail.com>
2025-11-07 13:30:33 +0000

    fc61823 Testes unitarios e de integracao

--- liquidjava-verifier/src/test/java/liquidjava/rj_language/ast/TestHashLitBoolean.java
+++ liquidjava-verifier/src/test/java/liquidjava/rj_language/ast/TestHashLitBoolean.java
@@ -1,13 +1,12 @@
 package liquidjava.rj_language.ast;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
-
-public class LiteralBooleanHashCodeTest {
+public class TestHashLitBoolean {
 
     @Test
-    public void sameValue_sameHashCode_andEquals() {
+    void sameValue_sameHashCode_andEquals() {
         LiteralBoolean a = new LiteralBoolean(true);
         LiteralBoolean b = new LiteralBoolean(true);
         assertEquals(a, b);
@@ -15,7 +14,7 @@ public class LiteralBooleanHashCodeTest {
     }
 
     @Test
-    public void differentValues_differentHashCodes() {
+    void differentValues_differentHashCodes() {
         LiteralBoolean a = new LiteralBoolean(true);
         LiteralBoolean b = new LiteralBoolean(false);
         assertNotEquals(a, b);
@@ -23,7 +22,7 @@ public class LiteralBooleanHashCodeTest {
     }
 
     @Test
-    public void clone_preservesEqualityAndHashCode() {
+    void clone_preservesEqualityAndHashCode() {
         LiteralBoolean a = new LiteralBoolean(false);
         LiteralBoolean c = (LiteralBoolean) a.clone();
         assertEquals(a, c);
