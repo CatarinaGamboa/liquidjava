60151ae431ba456b8dd9a20397e3b3eddbdc3043
Ricardo Costa <rcosta.ms358@gmail.com>
2025-11-11 11:56:09 +0000

    Add Diagnostic Examples for Testing

---
+++ liquidjava-example/src/main/java/testingInProgress/diagnostics/errors/GhostInvocationError.java
@@ -0,0 +1,11 @@
+package testingInProgress.diagnostics.errors;
+
+import liquidjava.specification.Ghost;
+import liquidjava.specification.StateRefinement;
+
+@Ghost("int size")
+public class GhostInvocationError {
+
+    @StateRefinement(to="size(this, this) == 0")
+    public void test() {}
+}
---
+++ liquidjava-example/src/main/java/testingInProgress/diagnostics/errors/RefinementError.java
@@ -0,0 +1,11 @@
+package testingInProgress.diagnostics.errors;
+
+import liquidjava.specification.Refinement;
+
+public class RefinementError {
+    
+    public static void main(String[] args) {
+        @Refinement("x > 0")
+        int x = -1;
+    }
+}
