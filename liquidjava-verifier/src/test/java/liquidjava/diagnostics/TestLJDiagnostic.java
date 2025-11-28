ab7882e7a6f185dd6a7ee03ad1de4cddf90b7450
Daniel Horta <dajh.horta@gmail.com>
2025-11-28 17:35:57 +0000

    LJDiagnostic getTitle method unit test implementation

---
+++ liquidjava-verifier/src/test/java/liquidjava/diagnostics/TestLJDiagnostic.java
@@ -0,0 +1,17 @@
+package liquidjava.diagnostics;
+
+import static org.junit.Assert.assertTrue;
+import liquidjava.diagnostics.LJDiagnostic;
+import org.junit.Test;
+
+public class TestLJDiagnostic {
+    @Test
+    public void testGetTitle() {
+        String title1 = "title";
+        String title2 = "fake title";
+        LJDiagnostic ljd = new LJDiagnostic(title1, null, null, null, null);
+
+        assertTrue(ljd.getTitle().equals(title1));
+        assertTrue(!ljd.getTitle().equals(title2));
+    }
+}
